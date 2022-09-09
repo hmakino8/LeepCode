8bd5cb6fc4779836c84295b0f259da28606dc111
Hiroaki Makino <8makino.42tokyo@gmail.com>
2022-09-09 12:01:20 +0900

    update

---
+++ 234.PalindromeLinkedList/234.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val int
+ 	Next *ListNode
+}
+
+type list struct {
+	head *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+	fmt.Println(head)
+	return true
+}
+
+func main () {
+	head *ListNode
+
+	head.val = 0
+	isPalindrome(ListNode)
+}
