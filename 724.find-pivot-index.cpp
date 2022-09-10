f91b9d811f0619cc9059e5fce69fc533eda5bec5
Hiroaki Makino <8makino.42tokyo@gmail.com>
2022-09-11 00:11:00 +0900

    update

--- 724.find-pivot-index.cpp
+++ 724.find-pivot-index.cpp
@@ -13,36 +13,3 @@ public:
 		return -1;
 	}
 };
-
-//class Solution {
-//public:
-//	int pivotIndex(vector<int> &nums) {
-//		for (int i = 0; i < nums.size(); i++) {
-//			if (i == 0) {
-//				int sum = 0;
-//				for (int j = 1; j < nums.size(); j++) {
-//					sum += nums[j];
-//				}
-//				if (sum == 0) {
-//					return 0;
-//				}
-//				else {
-//					continue ;
-//				}
-//			}
-//			int left = 0;
-//			int right = 0;
-//			for (int j = 0; j < i; j++) {
-//				left += nums[j];
-//			}
-//			for (int k = nums.size()-1; k > i; k--) {
-//				right += nums[k];
-//			}
-//			if (left == right) {
-//				return i;
-//			}
-//		}
-//		return -1;
-//	}
-//};
-//
